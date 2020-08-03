101526638a84d19cb763ae03c991782f59cec7b4
Váradi Richárd <ricsi19981007@gmail.com>
2020-08-03 08:03:46 +0200

    Major fixes due to domain update

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+var navHeight = $(".navbar").height() + 16;
+$("section").css('margin-top', navHeight);
+
+var footerHeight = $("#footer.row").height() + 16;
+$("#footer").css('margin-top', footerHeight);
