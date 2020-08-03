b3364fb330cf49527b2e9b511544252afd4c1e46
Váradi Richárd <ricsi19981007@gmail.com>
2020-08-03 08:20:13 +0200

    Fix sidebar and nav distance

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
-var navHeight = $(".navbar").height() + 16;
+var navHeight = $(".navbar").height() + 32;
 $("section").css('margin-top', navHeight);
+$("#accordion").css('margin-top', navHeight);
 
 var footerHeight = $("#footer.row").height() + 16;
 $("#footer").css('margin-top', footerHeight);
