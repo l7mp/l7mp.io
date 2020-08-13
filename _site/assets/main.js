d42d42366069a9699f5530cc824ddb5546943d21
Váradi Richárd <ricsi19981007@gmail.com>
2020-08-13 11:15:50 +0200

    Add table formatting

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -8,4 +8,13 @@ $("#footer").css('margin-top', footerHeight);
 
 $("table").each(function() {
     $(this).addClass("table table-striped");
+    let table = $(this).html()
+    console.log(table);
+    $(this).replaceWith('<div class="table-responsive"><table class="table table-striped">' + table + "</table></div>");
+    // $(this).appendTo('<div class="table-responsive></div>');
+    // var tableDiv = document.createElement("div");
+    // $(tableDiv).addClass("table-responsive");
+    // document.body.appendChild(tableDiv);
+    // tableDiv.append(JSON.stringify($(this)));
+    // tableDiv.appendChild($(this));
 });
