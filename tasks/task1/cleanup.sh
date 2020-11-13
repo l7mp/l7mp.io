dc5bc0a03191b60609201b9e753da3f4a4a34eb1
Richard Varadi <ricsi19981007@gmail.com>
2020-11-13 14:20:14 +0100

    Update cleanup.sh

--- tasks/task1/cleanup.sh
+++ tasks/task1/cleanup.sh
@@ -8,7 +8,7 @@ kubectl delete deployment.apps/transcoder-deployment
 kubectl delete configmap/transcoder-sidecar-config
 
 # Delete worker resources
-kubectl delete virtualservice.l7mp.io/worker-svc
+# kubectl delete virtualservice.l7mp.io/worker-svc
 kubectl delete target.l7mp.io/udp-cluster
 kubectl delete deployment.apps/worker-deployment
 kubectl delete configmap/worker-sidecar-config
