011a2e1e934e28806359e3f86f1278c40c7b6c85
Tamás Lévai <levaitamas@users.noreply.github.com>
2023-03-12 10:32:04 +0100

    fix: Fix a typo in STUNner description

--- src/data/HomeData.js
+++ src/data/HomeData.js
@@ -12,7 +12,7 @@ export const heroData = {
 
 export const stunnerData = {
     title: 'A Kubernetes media gateway for WebRTC',
-    subtitle: 'STUNner is a Kubernetes gateway designed for a single purpose; ingesting all you real-time media streams into a Kubernetes cluster in a controlled and standards-compliant way. Expose a single public port to the Internet, cover all NAT-traversal needs with a simple and versatile STUN/TURN server, and monitor resource usage in real time: STUNner catapults your WebRTC media services into the cloud-native era in an easy step.',
+    subtitle: 'STUNner is a Kubernetes gateway designed for a single purpose; ingesting all your real-time media streams into a Kubernetes cluster in a controlled and standards-compliant way. Expose a single public port to the Internet, cover all NAT-traversal needs with a simple and versatile STUN/TURN server, and monitor resource usage in real time: STUNner catapults your WebRTC media services into the cloud-native era in an easy step.',
     btnText: 'Get started',
     href: 'https://github.com/l7mp/stunner',
     image: StunnerImage,
