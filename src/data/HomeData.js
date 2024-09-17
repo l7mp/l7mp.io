fd31d013fc1b4dddcc3406eede5c5881eb800a04
Tamas Levai <levait@tmit.bme.hu>
2024-09-17 09:44:03 +0200

    Emphasize services on Home page

--- src/data/HomeData.js
+++ src/data/HomeData.js
@@ -4,16 +4,16 @@ import StunnerImage from '../assets/img/stunner.svg'
 export const heroData = {
     title: `Moving WebRTC backend services to Kubernetes`,
     subtitle:
-        'Deploy a containerized WebRTC infrastructure into Kubernetes and benefit from the unmatched lifecycle management features',
+        'We assist you to deploy a containerized WebRTC infrastructure into Kubernetes and benefit from the unmatched lifecycle management features.',
     btnText: 'Learn more',
-    href: '/#/about',
+    href: '/#/services',
     image: HeroImage,
 };
 
 export const stunnerData = {
     title: 'A Kubernetes media gateway for WebRTC',
-    subtitle: 'STUNner is a Kubernetes gateway designed for a single purpose; ingesting all your real-time media streams into a Kubernetes cluster in a controlled and standards-compliant way. Expose a single public port to the Internet, cover all NAT-traversal needs with a simple and versatile STUN/TURN server, and monitor resource usage in real time: STUNner catapults your WebRTC media services into the cloud-native era in an easy step.',
-    btnText: 'Get started',
-    href: 'https://github.com/l7mp/stunner',
+    subtitle: 'STUNner, our main product, is a Kubernetes gateway designed for a single purpose: ingesting all your real-time media streams into a Kubernetes cluster in a controlled and standards-compliant way. Expose a single public port to the Internet, cover all NAT-traversal needs with a simple and versatile STUN/TURN server, and monitor resource usage in real-time: STUNner catapults your WebRTC media services into the cloud-native era in an easy step',
+    btnText: 'Learn more',
+    href: '/#/products',
     image: StunnerImage,
   };
