b7d3c8cda7915182deea50c17b699ccfb4e2b4a4
Tamas Levai <levait@tmit.bme.hu>
2024-09-17 15:14:23 +0200

    Tweak video size on Products page

--- src/components/StunnerVideo.js
+++ src/components/StunnerVideo.js
@@ -7,7 +7,7 @@ const StunnerVideo = () => {
 		<h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
                     Demo Video
                 </h2>
-                <iframe height='480px' src="https://www.youtube.com/embed/5L1i5EW0HkI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className='w-3/5 content-center justify-center border border-gray-200 rounded-lg'></iframe>
+                <iframe width='640px' height='360px' src="https://www.youtube.com/embed/5L1i5EW0HkI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className='w-3/5 flex content-center items-center justify-center border border-gray-200 rounded-lg'></iframe>
             </div>
         </section>
     );
