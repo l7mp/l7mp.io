a5e8cb117d9c2c0d36c0aaaf754f3392b8a4b19f
Tamas Levai <levait@tmit.bme.hu>
2024-09-19 10:04:42 +0200

    Remove Contacts subtitle

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,7 @@ const Contact = () => {
     return (
         <section className="bg-white mt-[100px]">
             <div className="py-8 lg:py-8 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contacts</h2>
             </div>
             {contactData.map((item, index) => {
                 return (
